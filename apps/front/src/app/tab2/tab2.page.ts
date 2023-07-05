a9c7801e4e985ee075139c165f8ac02117d3f8ab
Marine PAYET <marine.payet3@gmail.com>
2023-07-05 15:53:17 +0200

    récup data from apiFood

--- apps/front/src/app/tab2/tab2.page.ts
+++ apps/front/src/app/tab2/tab2.page.ts
@@ -1,4 +1,42 @@
+// import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { KeyValue } from '@angular/common';
+
+// @Component({
+//   selector: 'app-tab2',
+//   templateUrl: 'tab2.page.html',
+//   styleUrls: ['tab2.page.scss']
+// })
+// export class Tab2Page {
+
+
+//   constructor( private http: HttpClient){
+
+//   }
+
+//     private configUrl = 'https://world.openfoodfacts.org/api/v2/product/3017620422003';
+//     products: any;
+//     currentProduct = undefined
+//   ​
+
+//     getProduct(){
+//       this.http.get<any>(`${this.configUrl}`).subscribe(data => {
+//         this.products = data['product'];
+//         this.currentProduct = this.products;
+//         console.log(this.products);
+//       });
+//     };
+//     ​
+//     ngOnInit(){
+//       this.getProduct();
+//       };
+
+// }
+
+
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { KeyValue } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +44,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  private configUrl = 'https://world.openfoodfacts.org/api/v2/product/3017620422003';
+  products: any;
+  currentProduct: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  getProduct() {
+    this.http.get<any>(this.configUrl).subscribe(data => {
+      this.products = data['product'];
+      this.currentProduct = this.products.brands;
+      console.log(this.currentProduct);
+    });
+  }
+
+  ngOnInit() {
+    this.getProduct();
+  }
 }
