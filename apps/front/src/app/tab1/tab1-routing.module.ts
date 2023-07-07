a6919e2ab53787ade73ffef42c80cae14b31099c
SALIMARGOUB <salimargoub@gmail.com>
2023-07-07 10:15:43 +0200

    ne merge pas

--- apps/front/src/app/tab1/tab1-routing.module.ts
+++ apps/front/src/app/tab1/tab1-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
-import { Tab2Page } from '../tab2/tab2.page';
 import { ProduitPage } from '../produit/produit.page';
 
 
